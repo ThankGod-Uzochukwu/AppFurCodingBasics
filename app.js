e5d316ecc50888155b9d9908f7866dd2e2e88e9f
TeeGee-TechBro <user.email>
2026-02-21 11:35:56 +0100

    basic in coding

---
+++ app.js
@@ -0,0 +1,51 @@
+
+// const gozie = "teegeeone"
+
+// const tg = "Techie"
+
+// let tg2 = "Techie2"
+
+// console.log(gozie)
+
+// const = constant variable, cannot be reassigned
+
+
+// appfurstring = "i am a string"
+// appfurstring2 = "i am also a string"
+
+// console.log(typeof(appfur))
+// console.log(typeof(appfurstring)) " "
+
+
+
+// this is a number for the appfur variable
+// appfur = 10
+
+// furapp = 40
+
+// console.log(appfur > furapp)
+// console.log(appfur !== furapp)
+// console.log(appfur < furapp)
+
+// console.log(typeof(appfur > furapp))
+
+// console.log(appfurstring + " " + appfurstring2)
+
+// console.log(`${appfurstring} ok ${appfurstring2} 10`)
+
+// appfur = 
+
+// console.log(appfur)
+
+// console.log(typeof(appfur))
+
+
+
+
+a = 105
+b = 20
+
+// console.log(a % b)
+
+console.log(a && b)
+// console.log(typeof(a + b))
